2b136bbbc4f5f5403d640db5580cc133d521f47b
Shayan Ahmad <sa6097@nyu.edu>
2024-05-02 00:31:19 +0400

    shifted to vite react, updated landing page

---
+++ abu-dhabi-eats/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/landingPage.css'
+import LandingPage from './pages/LandingPage';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
