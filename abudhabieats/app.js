c0cf4fa2185a3dc26d4867ae60c07d0d30cfcf86
Shayan Ahmad <sa6097@nyu.edu>
2024-05-07 00:08:10 +0400

    added all 7 createprofile pages

--- abudhabieats/app.js
+++ abudhabieats/app.js
@@ -18,9 +18,71 @@ app.get('/', (req, res) => {
     res.render('landingpage', { style: '/css/landingpage.css' });
 });
 
-// Route for the create profile page
+// Routes for the create profile pages
 app.get('/createprofile1', (req, res) => {
     res.render('createprofile1', { style: '/css/createprofile1.css' });  // Assuming you have separate CSS for this page
+});
+
+app.post('/createprofile2', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile2');
+});
+
+
+app.get('/createprofile2', (req, res) => {
+    res.render('createprofile2', { style: '/css/createprofile2.css' });
+});
+
+app.post('/createprofile3', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile3');
+});
+
+app.get('/createprofile3', (req, res) => {
+    res.render('createprofile3', { style: '/css/createprofile3.css' });
+});
+
+app.post('/createprofile4', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile4');
+});
+
+app.get('/createprofile4', (req, res) => {
+    res.render('createprofile4', { style: '/css/createprofile4.css' });
+});
+
+app.post('/createprofile5', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile5');
+});
+
+app.get('/createprofile5', (req, res) => {
+    res.render('createprofile5', { style: '/css/createprofile5.css' });
+});
+
+app.post('/createprofile6', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile6');
+});
+
+
+app.get('/createprofile6', (req, res) => {
+    res.render('createprofile6', { style: '/css/createprofile6.css' });
+});
+
+app.post('/createprofile7', (req, res) => {
+    // Process the form data here
+    // For now, just redirect to the createprofile2 page
+    res.redirect('/createprofile7');
+});
+
+app.get('/createprofile7', (req, res) => {
+    res.render('createprofile7', { style: '/css/createprofile7.css' });
 });
 
 
