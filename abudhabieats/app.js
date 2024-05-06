9d421cfee932fe160e407623dfffed071dfb812f
Shayan Ahmad <sa6097@nyu.edu>
2024-05-06 23:22:59 +0400

    added first createprofile page

--- abudhabieats/app.js
+++ abudhabieats/app.js
@@ -18,6 +18,12 @@ app.get('/', (req, res) => {
     res.render('landingpage', { style: '/css/landingpage.css' });
 });
 
+// Route for the create profile page
+app.get('/createprofile1', (req, res) => {
+    res.render('createprofile1', { style: '/css/createprofile1.css' });  // Assuming you have separate CSS for this page
+});
+
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
